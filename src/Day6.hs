f768c56c87e1ec75cb23cd178de297023c09670d
Jussi Vaihia <jussiava@gmail.com>
2021-12-06 14:23:08 +0200

    Day6

---
+++ src/Day6.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Day5 where
+
+import qualified Data.Text as T (splitOn, pack, unpack, lines)
+import qualified Data.Map.Strict as Map
+
+getInput = do
+    input <- T.lines . T.pack <$> readFile "input6.txt"
+    pure $ concat $ map ((read :: String -> Int) . T.unpack) . T.splitOn "," <$> input
+
+solve [] ys     = ys
+solve (x:xs) ys = case x of
+    0 -> solve xs (6:8:ys)
+    _ -> solve xs ((x-1):ys)
+
+day n t input
+    | n == t    = input
+    | otherwise = day (n+1) t (solve input [])
+
+day6_1 = do length . day 0 80 <$> getInput
+
+prepare [] ys = ys
+prepare (x:xs) ys = prepare xs (Map.insertWith (+) x 1 ys)
+
+solve' [] ys           = ys
+solve' m@((k,v):xs) ys = case k of
+    0 -> solve' xs (Map.insertWith (+) 6 v $ Map.insertWith (+) 8 v ys)
+    _ -> solve' xs (Map.insertWith (+) (k-1) v ys)
+
+day' n t input
+    | n == t    = input
+    | otherwise = day' (n+1) t (solve' (reverse $ Map.assocs input) Map.empty)
+
+day6_2 = do
+    input <- getInput
+    pure $ day' 0 256 (prepare input Map.empty)
+
